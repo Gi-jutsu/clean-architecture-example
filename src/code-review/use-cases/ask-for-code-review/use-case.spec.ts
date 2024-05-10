9713a5c7a8914f43a41d4f228535138875c4c807
Dylan De Sousa <dylan.de-sousa@arsen.co>
2024-05-10 11:59:24 +0200

    refactor: rename AskForCodeReview to AskForCodeReviewUseCase

--- src/code-review/use-cases/ask-for-code-review/use-case.spec.ts
+++ src/code-review/use-cases/ask-for-code-review/use-case.spec.ts
@@ -1,14 +1,14 @@
 import { describe, expect, it } from "vitest";
-import { AskForCodeReview } from "./use-case.js";
+import { AskForCodeReviewUseCase } from "./use-case.js";
 import { AskForCodeReviewCommand } from "./command.js";
 import { CodeReviewRequest } from "../../domain/code-review-request/aggregate-root.js";
 import { InMemoryCodeReviewRequestRepository } from "../../infrastructure/repositories/doubles/in-memory-code-review-request.repository.js";
 
-describe("AskForCodeReview", () => {
+describe("AskForCodeReviewUseCase", () => {
   it("should create a new CodeReviewRequest", () => {
     // Arrange
     const repository = new InMemoryCodeReviewRequestRepository();
-    const useCase = new AskForCodeReview(repository);
+    const useCase = new AskForCodeReviewUseCase(repository);
 
     const command = new AskForCodeReviewCommand({
       tags: ["NodeJS", "Typescript", "Clean Architecture"],
