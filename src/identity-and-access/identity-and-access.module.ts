385c32eec5a9673752d6f196546333dd0cd81cb3
Dylan De Sousa <dylan.de-sousa@arsen.co>
2024-08-13 04:42:20 +0200

    feat(iam): add fake read-me query

--- src/identity-and-access/identity-and-access.module.ts
+++ src/identity-and-access/identity-and-access.module.ts
@@ -2,6 +2,8 @@ import {
   type JwtService,
   JwtServiceToken,
 } from "@identity-and-access/infrastructure/services/jwt.service.js";
+import { ReadMeHttpController } from "@identity-and-access/use-cases/read-me/http.controller.js";
+import { ReadMeUseCase } from "@identity-and-access/use-cases/read-me/use-case.js";
 import { SignInWithOAuthProviderHttpController } from "@identity-and-access/use-cases/sign-in-with-oauth-provider/http.controller.js";
 import { SignInWithOAuthProviderUseCase } from "@identity-and-access/use-cases/sign-in-with-oauth-provider/use-case.js";
 import { Module } from "@nestjs/common";
@@ -9,6 +11,7 @@ import { Module } from "@nestjs/common";
 @Module({
   controllers: [
     /** HTTP Controllers */
+    ReadMeHttpController,
     SignInWithOAuthProviderHttpController,
   ],
   providers: [
@@ -19,6 +22,7 @@ import { Module } from "@nestjs/common";
     },
 
     /** Use Cases */
+    ReadMeUseCase,
     {
       provide: SignInWithOAuthProviderUseCase,
       useFactory: (jwt: JwtService) => new SignInWithOAuthProviderUseCase(jwt),
---
+++ src/identity-and-access/use-cases/read-me/use-case.ts
@@ -0,0 +1,8 @@
+export class ReadMeUseCase {
+  async execute() {
+    return {
+      id: "1",
+      email: "john.doe@email.com",
+    };
+  }
+}
