58dd2a3287da5088a7784d534d5a0247afcef1ab
Dylan De Sousa <dylan.de-sousa@arsen.co>
2024-12-13 21:38:11 +0100

    refactor(core): remove dead code

--- src/core/primitives/aggregate-root.base.ts
+++ src/core/primitives/aggregate-root.base.ts
@@ -4,18 +4,10 @@ import { Entity } from "./entity.base.js";
 export abstract class AggregateRoot<
   Properties extends Record<keyof Properties, unknown>
 > extends Entity<Properties> {
-  // @TODO: Benchmark the performance on high-scale events (consider using a queue)
   private readonly _events: DomainEvent[] = [];
 
   protected commit(event: DomainEvent) {
     this._events.push(event);
-  }
-
-  get properties() {
-    return Object.freeze({
-      id: this.id,
-      ...this._properties,
-    });
   }
 
   pullDomainEvents() {
